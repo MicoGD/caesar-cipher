a500eba4b6a5d040fb4e92be3e76bafbc005f1dd
robotomize <robotomize@gmail.com>
2015-10-05 00:19:05 +1000

    upd some

--- src/example.php
+++ src/example.php
@@ -1,10 +1,7 @@
 <?php
 
-namespace CaesarCipher;
+namespace CaesarCipherru;
 
-use CaesarCipher\CaesarCipherRu;
-
-include 'CaesarCipher.php';
 include 'CaesarCipherRu.php';
 
 /**
@@ -13,13 +10,13 @@ include 'CaesarCipherRu.php';
 $tt = new CaesarCipherRu();
 print $tt->encode('окейгугл', 3) . PHP_EOL;
 
-print $tt->crack('юйбьхуфдтацъьлъюйцшхщуцюхдлщобер') . PHP_EOL;
+print $tt->crack('') . PHP_EOL;
 
 /**
  * Some iterate matrix
  */
 foreach(range(1, 32) as $values) {
-    print sprintf('key: %s - value: %s', $values, $tt->encode('правильныйответпрочислопинескажу', $values) . PHP_EOL);
+    print sprintf('key: %s - value: %s', $values, $tt->encode('', $values) . PHP_EOL);
 }
 
 
---
+++ tests/CaesarCipherTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace test;
+
+use CaesarCipherRu\CaesarCipherRu;
+
+class CaesarCipherTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEncode()
+    {
+
+    }
+
+
+    public function testDecode()
+    {
+
+    }
+
+    public function testCrack()
+    {
+
+    }
+}
