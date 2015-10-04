e2eef8814f47333513f6793607e8ae67614d1c06
robotomize <robotomize@gmail.com>
2015-10-05 00:11:48 +1000

    up

--- src/CaesarCipher/example.php
+++ src/CaesarCipher/example.php
@@ -13,13 +13,13 @@ include 'CaesarCipherRu.php';
 $tt = new CaesarCipherRu();
 print $tt->encode('окейгугл', 3) . PHP_EOL;
 
-print $tt->crack('юйбьхуфдтацъьлъюйцшхщуцюхдлщобер') . PHP_EOL;
+print $tt->crack('') . PHP_EOL;
 
 /**
  * Some iterate matrix
  */
 foreach(range(1, 32) as $values) {
-    print sprintf('key: %s - value: %s', $values, $tt->encode('правильныйответпрочислопинескажу', $values) . PHP_EOL);
+    print sprintf('key: %s - value: %s', $values, $tt->encode('', $values) . PHP_EOL);
 }
 
 
